4f4e6b7f0c3763623a399c4c0d8b76b6cb631592
matisse <Matisse.machand@edu.devinci.fr>
2024-02-25 15:37:02 +0100

    data

--- app/src/main/java/com/example/movie2/DataProvider.kt
+++ app/src/main/java/com/example/movie2/DataProvider.kt
@@ -1,4 +1,9 @@
 package com.example.movie2
 
-class DataProvider {
+object DataProvider {
+    val seriesList = listOf(
+        Serie(1, "Agents of Shield", "Marvel", 136, 2013, R.drawable.agents_of_shield),
+        Serie(2, "Arrow", "DC Comics", 170, 2012, R.drawable.arrow),
+        // ... Ajoutez d'autres séries ici
+    )
 }
--- app/src/main/java/com/example/movie2/Serie.kt
+++ app/src/main/java/com/example/movie2/Serie.kt
@@ -8,10 +8,3 @@ data class Serie(
     val year: Int,
     val imageResourceId: Int
 )
-object DataProvider {
-    val seriesList = listOf(
-        Serie(1, "Agents of Shield", "Marvel", 136, 2013, R.drawable.agents_of_shield),
-        Serie(2, "Arrow", "DC Comics", 170, 2012, R.drawable.arrow),
-        // ... Ajoutez d'autres séries ici
-    )
-}
