05f64058fea30fac7964fa2851f6e1d31802a80e
raph.hge <raph.hge@gmail.com>
2024-02-25 19:19:01 +0100

    nav bottom modif

--- app/src/main/java/com/example/movie2/MainActivity.kt
+++ app/src/main/java/com/example/movie2/MainActivity.kt
@@ -2,43 +2,24 @@ package com.example.movie2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
 
-        navView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    // Naviguer vers le fragment d'accueil
-                    true
-                }
-                R.id.navigation_comics -> {
-                    // Naviguer vers le fragment de comics
-                    true
-                }
-                R.id.navigation_movies -> {
-                    // Naviguer vers le fragment de films
-                    true
-                }
-                R.id.navigation_search -> {
-                    // Naviguer vers le fragment de recherche
-                    true
-                }
-                R.id.navigation_series -> {
-                    // Naviguer vers le fragment de séries
-                    true
-                }
-                else -> false
-            }
+        findViewById<ImageButton>(R.id.homeButton).setOnClickListener {
+            navController.navigate(R.id.homeFragment)
         }
-        // Assurez-vous que la navigation est configurée correctement avec le NavController
-        navView.setupWithNavController(navController)
+
+        // Répétez pour chaque bouton/icon
     }
 }
