f35440927ec187d0231129a242a40f6105a4e7ab
matisse <Matisse.machand@edu.devinci.fr>
2024-02-25 16:30:43 +0100

    detail fragment

--- app/src/main/java/com/example/movie2/SeriesDetailFragment.kt
+++ app/src/main/java/com/example/movie2/SeriesDetailFragment.kt
@@ -1,4 +1,28 @@
 package com.example.movie2
 
-class SeriesDetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SeriesDetailFragment : Fragment() {
+
+    // Vous pouvez utiliser un argument pour passer les détails de la série au fragment
+    // Cet exemple ne contient pas la logique pour récupérer ces données.
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate le layout pour ce fragment
+        return inflater.inflate(R.layout.fragment_series_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Ici, vous récupéreriez les détails de la série passés en argument et les afficheriez
+        // à l'aide des vues définies dans votre layout fragment_series_detail.xml
+    }
 }
