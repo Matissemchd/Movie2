2d22ff1b3f1e4da5287ec715fc01e77f761636f1
matisse <Matisse.machand@edu.devinci.fr>
2024-02-25 16:15:12 +0100

    push

--- app/src/main/java/com/example/movie2/SeriesListFragment.kt
+++ app/src/main/java/com/example/movie2/SeriesListFragment.kt
@@ -1,61 +1,27 @@
 package com.example.movie2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.movie2.placeholder.PlaceholderContent
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-/**
- * A fragment representing a list of Items.
- */
 class SeriesListFragment : Fragment() {
 
-    private var columnCount = 1
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
+    private lateinit var seriesAdapter: SeriesAdapter
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_series_list_list, container, false)
-
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = MyItemRecyclerViewAdapter(PlaceholderContent.ITEMS)
-            }
-        }
+        val view = inflater.inflate(R.layout.fragment_series_list, container, false)
+        recyclerView = view.findViewById(R.id.series_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        seriesAdapter = SeriesAdapter(DataProvider.seriesList)
+        recyclerView.adapter = seriesAdapter
         return view
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            SeriesListFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
     }
 }
